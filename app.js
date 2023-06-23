34e49651ee7cf8ab195b033d6b18e5b01ec273f3
Joel Osei <oseij96@gmail.com>
2023-06-23 14:49:50 +0100

    Update

--- app.js
+++ app.js
@@ -28,7 +28,12 @@ const MongoStore = require('connect-mongo');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp_camp';
 
 mongoose.set('strictQuery', true)
-mongoose.connect(dbUrl);
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
